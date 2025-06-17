884f4bf111a319ea44319fc07df2c8b72b30a549
Nisargp17 <nisargsudhir6028@gmail.com>
2025-06-17 22:04:53 +0530

    Inventory Management Update commit

--- inventory-management/src/util/DBConnection.java
+++ inventory-management/src/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/inventory_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "Nisarg@30134!";
+    private static final String PASSWORD = "Nisarg@30314!";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
