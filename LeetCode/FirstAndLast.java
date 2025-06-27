86eb9bc2afd10c2fd7f7631d6fea4a399124a1cb
Nisargp17 <nisargsudhir6028@gmail.com>
2025-06-27 22:04:35 +0530

    Leetcode commit

--- LeetCode/FirstAndLast.java
+++ LeetCode/FirstAndLast.java
@@ -1,5 +1,39 @@
 package Projects.LeetCode;
 
 public class FirstAndLast {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 2, 3, 4, 4, 5, 6 };
+        System.out.println(InnerFirstAndLast.findElement(arr, 4));
+    }
+}
 
+class InnerFirstAndLast {
+    static int[] findElement(int[] arr, int val) {
+        int first = findIndex(arr, val, true);
+        int last = findIndex(arr, val, false);
+        System.out.println(first);
+        return new int[] { first, last };
+    }
+
+    static int findIndex(int[] arr, int val, boolean isFirst) {
+        int si = 0, ei = arr.length - 1;
+        int index = -1;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (arr[mid] == val) {
+                index = mid;
+                if (isFirst) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
+                }
+            } else if (arr[mid] < val) {
+                si = mid + 1;
+            } else {
+                ei = mid - 1;
+            }
+
+        }
+        return index;
+    }
 }
